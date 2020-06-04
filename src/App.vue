97742ad990014e0148d7770316ce0db0ebc13d42
Serge Tymoshenko <sergiy.tymoshenko@leongaming.net>
2020-06-04 18:54:52 +0300

    WIP: Choice and Select

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span>{{state.current}}</span>
+    <hr>
+    <Select v-model="state.current" :values="values" />
+  </div>
+</template>
+
+<script lang="ts">
+import Select from './components/Select.vue'
+import { reactive, watch } from 'vue'
+
+export default {
+  name: 'App',
+  components: { Select },
+
+  setup () {
+    const state = reactive({ current: ['foo', 'tri'] })
+    const values = ['foo', 'bar', 'baz', 'tri']
+
+    return { state, values }
+  },
+}
+</script>
---
+++ src/components/RenderlessCounter.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { h, reactive, computed } from 'vue'
+
+export default {
+  setup(props, { slots }) {
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2)
+    })
+
+    function increment () {
+      state.count++
+    }
+
+    return () => h('div', slots.default({
+      count: state.count,
+      double: state.double,
+      increment,
+    }))
+  }
+}
+</script>
